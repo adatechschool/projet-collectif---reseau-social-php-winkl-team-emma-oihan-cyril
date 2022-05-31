05adbd746f8102e3f81a605dc4f4381b80c35d97
Cyril <lgcyriltt@gmail.com>
2022-05-31 16:15:06 +0200

    settings ok

--- settings.php
+++ settings.php
@@ -67,12 +67,22 @@
                  */
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puiseffacer la ligne ci-dessous
                 
-                while ($user = $lesInformations->fetch_assoc(){
-                    echo "<pre>" . print_r($user, 1) . "</pre>";
+                while ($user = $lesInformations->fetch_assoc()){
+                    //echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>                
                 <article class='parameters'>
                     <h3>Mes paramètres</h3>
                     <dl>
+                        <dt>Pseudo</dt>
+                        <dd><?php echo $user['alias']?></dd>
+                        <dt>Email</dt>
+                        <dd><?php echo $user['email']?></dd>
+                        <dt>Nombre de message</dt>
+                        <dd><?php echo $user['totalpost']?></dd>
+                        <dt>Nombre de "J'aime" donnés </dt>
+                        <dd><?php echo $user['totalgiven']?></dd>
+                        <dt>Nombre de "J'aime" reçus</dt>
+                        <dd><?php echo $user['totalrecieved']?></dd>
                     </dl>
 
                 </article>
