fdea47bbee04f6cd90af93f69d8c8f43fffbbd8b
Oihan75 <oihanernaga@gmail.com>
2022-06-08 16:51:12 +0200

    bouton follow

--- news.php
+++ news.php
@@ -104,26 +104,6 @@
                     exit();
                 }
 
-                function follow() {
-                    echo "hello";
-                    // $lInstructionSql = "INSERT INTO followers "
-                    //             . "(id, followed_user_id, following_user_id) "
-                    //             . VALUES (NULL, $post['id'], $_SESSION['connected_id']) ; // fin sql
-                    //             ;  // fermer php
-                    //             // INSERT INTO followers (id, followed_user_id, following_user_id)
-                    //             // VALUES (NULL, 3,5);
-                    //     echo $lInstructionSql;
-                    //     // Etape 5 : execution
-                    //     $ok = $mysqli->query($lInstructionSql);
-                    //     if ( ! $ok)
-                    //     {
-                    //         echo "Impossible de suivre cette personne : " . $mysqli->error;
-                    //     } else
-                    //     {
-                    //         echo " Vous suivez cette personne :" . $post['author_name'];
-                    //         //exit();
-                    //     }
-                    }
 
                 // Etape 3: Parcourir ces données et les ranger bien comme il faut dans du html
                 // NB: à chaque tour du while, la variable post ci dessous reçois les informations du post suivant.
@@ -147,9 +127,35 @@
                             <div class="col-12">
                                 <!-- on affiche suivre seulement si ce n'est pas son compte à lui. -->
                                 <?php if($_SESSION['connected_id'] != $post['id']) { ?>
-                                 <button type="button" 
+                                 <!-- <button type="button" 
                                 class="btn btn-success" 
-                                onclick="follow()">Suivre</button>
+                                onclick="follow()">Suivre</button> -->
+                                <form method="post" action="news.php">
+                                <input type="hidden" name="hello" value="true" />
+                                <input type="button" value="Suivre" />
+                                </form>
+
+                                <?php if ($_POST['hello'] != "true") { ?>
+                                <?php $lInstructionSql = "INSERT INTO followers "
+                                . "(id, followed_user_id, following_user_id) "
+                                . "VALUES (NULL, "
+                                . $post['id'] . ", "
+                                 . $_SESSION['connected_id'] .")"; // fin sql
+                                ;  // fermer php
+                                // INSERT INTO followers (id, followed_user_id, following_user_id)
+                                // VALUES (NULL, 3,5);
+                        //echo $lInstructionSql;
+                        // Etape 5 : execution
+                        $ok = $mysqli->query($lInstructionSql);
+                        if ( ! $ok)
+                        {
+                            echo "Impossible de suivre cette personne : " . $mysqli->error;
+                        } else
+                        {
+                            echo " Vous suivez cette personne :" . $post['author_name'];
+                            //exit();
+                        } ?>
+                                <?php } ?>
                                 <?php } ?>
                         </address>
                         <div>
